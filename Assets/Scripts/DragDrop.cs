2676f1ec07749158c5a0a772d42714097a386797
Armando García Paniagua <62965639+ArmandGP07@users.noreply.github.com>
2021-01-26 01:01:20 -0600

    Drag and Drop
    Se logró hacer un Drag and Drop de la resistencia y fuente de poder

---
+++ Assets/Scripts/DragDrop.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerUpHandler
+{
+
+    [SerializeField] public Canvas canvas;
+
+    public Componente copia;
+
+    private RectTransform rectTransform;
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnBeginDrag");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag");
+        rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("OnPointerDown");
+
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        //Instantiate(copia, new Vector2(179, 96), transform.rotation);
+    }
+}
