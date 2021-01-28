1871ff1ba6d8de1f5d5da18bec7967e0ee1d92cb
Joel Gómez <62965696+Joel-Araya@users.noreply.github.com>
2021-01-27 23:11:20 -0600

    Arreglos en el programa

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -3,49 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerUpHandler
+public class DragDrop : MonoBehaviour
 {
 
-    [SerializeField] public Canvas canvas;
+    //[SerializeField] public Canvas canvas;
     public bool simulación;
     public Componente generado;
     public string tipoGenerado;
     Vector3 posOriginal; 
 
-    private RectTransform rectTransform;
 
     private void Awake()
     {
-        rectTransform = GetComponent<RectTransform>();
-        posOriginal = new Vector3(rectTransform.position.x, rectTransform.position.y, rectTransform.position.z);
+        posOriginal = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         
     }
-    public void OnBeginDrag(PointerEventData eventData)
-    {
-        //Debug.Log("OnBeginDrag");
-    }
 
-    public void OnDrag(PointerEventData eventData)
+    public void OnMouseDrag() 
     {
         if (!FindObjectOfType<BotonCambioModo>().modoSimulacion)
         {
             //Debug.Log("OnDrag");
-            rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            //rectTransform.anchoredPosition += eventData.delta / canvas.scaleFactor;
+            transform.position = Input.mousePosition;
         }
     }
 
-    public void OnEndDrag(PointerEventData eventData)
-    {
-        //Debug.Log("OnEndDrag");
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        //Debug.Log("OnPointerDown");
-
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
+    public void OnMouseUp()
     {
         if (!FindObjectOfType<BotonCambioModo>().modoSimulacion)
         {
@@ -53,14 +37,14 @@ public class DragDrop : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, I
 
             if (tipoGenerado == "Resistencia")
             {
-                Instantiate(generado, new Vector2(rectTransform.position.x, rectTransform.position.y), transform.rotation);
-                rectTransform.SetPositionAndRotation(posOriginal, transform.rotation);
+                Instantiate(generado, new Vector2(transform.position.x, transform.position.y), transform.rotation);
+                transform.SetPositionAndRotation(posOriginal, transform.rotation);
 
             }
             else if (tipoGenerado == "Fuente")
             {
-                Instantiate(generado, new Vector2(rectTransform.position.x, rectTransform.position.y), transform.rotation);
-                rectTransform.SetPositionAndRotation(posOriginal, transform.rotation);
+                Instantiate(generado, new Vector2(transform.position.x, transform.position.y), transform.rotation);
+                transform.SetPositionAndRotation(posOriginal, transform.rotation);
             }
         }
     }
